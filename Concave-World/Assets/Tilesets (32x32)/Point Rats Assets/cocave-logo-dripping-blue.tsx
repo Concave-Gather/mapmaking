c0d9ccd14d756b693e14625a2efe8ead023527a0
point-rat <100729185+point-rat@users.noreply.github.com>
2022-04-05 22:47:18 +0200

    Point rat updates
    - added octomob boardroom
    - added some tilesets
    - updated Wagmi Beach

---
+++ Concave-World/Assets/Tilesets (32x32)/Point Rats Assets/cocave-logo-dripping-blue.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="cocave-logo-dripping-blue" tilewidth="32" tileheight="32" tilecount="6" columns="3">
+ <image source="cocave-logo-dripping-blue.png" width="96" height="64"/>
+</tileset>
